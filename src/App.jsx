ee18e109676141b290bcba86e2b03ef4295621f0
Vladislav <104913853+Adwasar@users.noreply.github.com>
2023-11-08 21:30:03 +0200

    feat: style for project blocks

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
         </div>
       </header>
       <main>
-        <section id="about" className="vh-100 d-flex align-items-center bg-grey">
+        <section id="about" className="min-vh-100 d-flex align-items-center bg-grey">
           <div className="container">
             <h1>Front-End React Developer</h1>
             <p>
@@ -60,8 +60,8 @@ function App() {
             </ul>
           </div>
         </section>
-        <section id="projects" className="vh-100 d-flex align-items-center">
-          <div className="container mt-5">
+        <section id="projects" className="min-vh-100 d-flex align-items-center">
+          <div className="container my-5">
             <h2>Projects</h2>
             <div className="projects text-center ">
               <div className="row bg-grey project shadow-lg px-5 py-5 mt-5 align-items-center">
@@ -70,7 +70,31 @@ function App() {
                     <img src={HayBossImg}></img>
                   </a>
                 </div>
-                <div className="col-12 col-md-7 px-0 px-md-5 pt-5 pt-md-0">
+                <div className="col-12 col-md-7 px-0 px-md-5 mt-5 mt-md-0">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
+                  eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+                  montes, nascetur ridiculus mus. Donec quam felis,
+                </div>
+              </div>
+              <div className="row bg-grey project shadow-lg px-5 py-5 mt-5 align-items-center">
+                <div className="project__img p-0 col-12 col-md-5 mt-5 mt-md-0 order-2">
+                  <a href="https://marketplace-landing-29h.pages.dev/">
+                    <img src={HayBossImg}></img>
+                  </a>
+                </div>
+                <div className="col-12 col-md-7 px-0 px-md-5 order-1 ">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
+                  eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+                  montes, nascetur ridiculus mus. Donec quam felis,
+                </div>
+              </div>
+              <div className="row bg-grey project shadow-lg px-5 py-5 mt-5 align-items-center">
+                <div className="project__img p-0 col-12 col-md-5 order-1">
+                  <a href="https://marketplace-landing-29h.pages.dev/">
+                    <img src={HayBossImg}></img>
+                  </a>
+                </div>
+                <div className="col-12 col-md-7 px-0 px-md-5 mt-5 mt-md-0 order-2">
                   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
                   eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
                   montes, nascetur ridiculus mus. Donec quam felis,
