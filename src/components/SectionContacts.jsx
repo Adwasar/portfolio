7ce67058927c0132743b4092b4d33906d634c607
Vladislav <104913853+Adwasar@users.noreply.github.com>
2023-11-15 11:12:34 +0200

    refactor: projects data

--- src/components/SectionContacts.jsx
+++ src/components/SectionContacts.jsx
@@ -6,32 +6,32 @@ function SectionContacts() {
           <h2 className="display-2">Contacts</h2>
           <ul className="mt-5">
             <li>
-              <a href="tel:+380954976848">
+              <a className="d-flex flex-wrap" href="tel:+380954976848">
                 <span className="fw-semi me-3">Phone:</span>
                 +380 (95) 49 76 848
               </a>
             </li>
             <li>
-              <a href="https://t.me/adwasarik">
+              <a className="d-flex flex-wrap" href="https://t.me/adwasarik">
                 <span className="fw-semi me-3">Telegram:</span>@adwasarik
               </a>
             </li>
             <li>
-              <a href="mailto:adwasarik@gmail.com">
+              <a className="d-flex flex-wrap" href="mailto:adwasarik@gmail.com">
                 <span className="fw-semi me-3">E-mail:</span>
                 adwasarik@gmail.com
               </a>
             </li>
             <li>
-              <a href="https://github.com/Adwasar">
+              <a className="d-flex flex-wrap" href="https://github.com/Adwasar">
                 <span className="fw-semi me-3">GitHub:</span>
                 github.com/Adwasar
               </a>
             </li>
-            <li className="d-flex flex-wrap">
-              <a href="https://linkedin.com/in/adwasar">
+            <li>
+              <a className="d-flex flex-wrap" href="https://linkedin.com/in/adwasar">
                 <span className="fw-semi me-3">Linkedin:</span>
-                linkedin.com/in/adwasar
+                <span>linkedin.com/in/adwasar</span>
               </a>
             </li>
           </ul>
---
+++ src/data/projects.js
@@ -0,0 +1,50 @@
+import HayBossImg from '../assets/img/HeyBoss.png';
+import ReactPizza from '../assets/img/react-pizza.png';
+import SneakerStore from '../assets/img/sneakers-store.png';
+import RsHealthy from '../assets/img/rs-healthy.png';
+
+const projects = [
+  {
+    imgSrc: HayBossImg,
+    demoUrl: 'https://marketplace-landing-29h.pages.dev/',
+    repoUrl: '',
+    title: 'Team project: "HeyBoss"',
+    description: 'Landing for marketplace',
+    functional: 'Product description, Feedback form, Localization of 2 languages',
+    tools:
+      'React, JavaScript, Bootstrap, React, Router, React Spinners, react-i18next, REST APIs, SCSS, ESlint, Prettier',
+  },
+  {
+    imgSrc: ReactPizza,
+    demoUrl: 'https://react-pizza-70179.netlify.app/',
+    repoUrl: 'https://github.com/Adwasar/react-pizza',
+    title: 'Project "Pizzas store"',
+    description: 'Pizzas online store',
+    functional: 'add cards on cart (storage on server), filter cards by name or category',
+    tools:
+      'React, JavaScript, Redux toolkit, React Router, React Spinners, React Skeleton, Axios, REST APIs, SCSS, ESlint, Prettier',
+  },
+  {
+    imgSrc: SneakerStore,
+    demoUrl: 'https://sneakers-store-3950213.netlify.app',
+    repoUrl: 'https://github.com/Adwasar/react-sneakers',
+    title: 'Project "Sneakers store"',
+    description: 'Sneakers online store',
+    functional:
+      'add cards on cart (storage on server), add cards on favorites, filter by name, order history on the user page (storage on server)',
+    tools:
+      'React, JavaScript, React Router, React Spinners, React Skeleton, Axios, REST APIs, SCSS, ESlint, Prettier',
+  },
+  {
+    imgSrc: RsHealthy,
+    demoUrl: 'https://rs-clone-adwasar-pnmrvvtl-zhannach.netlify.app/',
+    repoUrl: 'https://rs-clone-adwasar-pnmrvvtl-zhannach.netlify.app/',
+    title: 'Team project "RS-Healthy"',
+    description: 'The application that helps with meal planning',
+    functional:
+      'after user input some values about himself our app can propose personal data for this user about health and propose meal plan for user.',
+    tools: 'React, TypeScript, REST APIs, SCSS, Jest, ESlint, Prettier',
+  },
+];
+
+export default projects;
